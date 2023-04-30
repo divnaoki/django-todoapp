cc8ee84416963471d53971cafe5e42db17dc7901
divnaoki <naokiimai1@gmail.com>
2023-04-30 20:32:41 +0900

    4/30 Todo一覧のviews.py, templates作成

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import TodoItem
 
 # Create your views here.
+
+def index(request):
+  todo_list = TodoItem.objects.all()
+  context = {'todo_list': todo_list}
+  return render(request, 'todoapp/index.html', context)
