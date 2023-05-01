81b12a31b572b625c9e2473528cebab1ce327b8b
divnaoki <naokiimai1@gmail.com>
2023-05-01 20:16:44 +0900

    5/1 Todo追加処理追加

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import index, craete_todo
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add/', views.craete_todo, name='craete_todo'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import TodoItem
+from .forms import TodoForm
 
 # Create your views here.
 
@@ -7,3 +8,15 @@ def index(request):
   todo_list = TodoItem.objects.all()
   context = {'todo_list': todo_list}
   return render(request, 'todoapp/index.html', context)
+
+def craete_todo(request):
+  if request.method == 'POST':
+    form = TodoForm(request.POST)
+    if form.is_valid():
+      title = form.cleaned_data['title']
+      new_item = TodoItem(title=title)
+      new_item.save()
+      return redirect('index')
+  else:
+    form = TodoForm()
+  return render(request, 'todoapp/add.html', {'form': form})
